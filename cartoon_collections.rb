32f5db9eb54e7b3e04f66fbe6713555b2fa7e4cb
sasha walker <sashawalker31@gmail.com>
2020-01-31 14:32:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(array)# code an argument here
   end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
   answer = false
   long_planteer_calls.each do |call|
