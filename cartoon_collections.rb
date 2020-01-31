48d199dfdea4b57679dcc3228f794c0c65428056
sasha walker <sashawalker31@gmail.com>
2020-01-31 14:11:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,7 @@ def roll_call_dwarves(names)
   names = ["Doc", "Dopey", "Bashful", "Grumpy"]
   names.each_with_index(1) do |item, name|
     puts #{index} #{name}
+  end
 end
 
 def summon_captain_planet# code an argument here
