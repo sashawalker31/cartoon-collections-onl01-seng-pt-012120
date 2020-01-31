0a5345a10cdea58313b7c2b72aee3937678be8e8
sasha walker <sashawalker31@gmail.com>
2020-01-31 14:30:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves(dwarfs)
-  dwawrfs.each_with_index do |name, index|
+def roll_call_dwarves(array)
+  array.each_with_index do |name, index|
     puts "#{index + 1} #{name}"
   end
 end
