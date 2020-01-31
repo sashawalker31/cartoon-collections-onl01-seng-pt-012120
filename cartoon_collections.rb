8c02c6ffc4bed8400a7b5fcc6029f327665fd7f8
sasha walker <sashawalker31@gmail.com>
2020-01-31 14:33:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,10 @@ def summon_captain_planet(array)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
-  
+  array.any? do |word| 
+     word.length > 4 
+   end 
+ end 
 end
 
 def find_the_cheese# code an argument here
