dad72eacd1a948db24184093132e0a46da249d9c
sasha walker <sashawalker31@gmail.com>
2020-01-31 14:33:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,6 @@ def long_planeteer_calls(array)# code an argument here
      word.length > 4 
    end 
  end 
-end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
